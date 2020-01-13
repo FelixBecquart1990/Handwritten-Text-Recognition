55fdc9da6acf3463a44d84d66c86f7444abf7837
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-01-13 09:44:00 +0000

    last

--- model.py
+++ model.py
@@ -11,6 +11,20 @@ import os
 import tensorflow as tf
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
 
+import tensorflow as tf
+from tensorflow.keras import backend as K
+from tensorflow.keras import Model
+from tensorflow.keras.optimizers import RMSprop, Adam
+from tensorflow.keras.utils import Progbar
+
+from tensorflow.keras.callbacks import CSVLogger, TensorBoard, ModelCheckpoint
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
+from tensorflow.keras.constraints import MaxNorm
+
+from tensorflow.keras.layers import Conv2D, Bidirectional, LSTM, GRU, Dense
+from tensorflow.keras.layers import Dropout, BatchNormalization, LeakyReLU, PReLU
+from tensorflow.keras.layers import Input, MaxPooling2D, Reshape
+
 class FullGatedConv2D(Conv2D):
     """Gated Convolutional Class"""
 
@@ -96,7 +110,7 @@ def flor(input_size, d_model, learning_rate):
     if learning_rate is None:
         learning_rate = 5e-4
 
-    optimizer = tf.keras.optimizers.RMSprop(learning_rate=learning_rate)
+    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
 
     return (input_data, output_data, optimizer)
 
@@ -150,3 +164,66 @@ def get_callbacks(checkpoint, monitor="val_loss", verbose=0):
         ]
 
         return callbacks
+    
+def minh_model(input_size, d_model, learning_rate=3e-4):
+    """
+    Convolucional Recurrent Neural Network by Puigcerver et al.
+    Reference:
+        Joan Puigcerver.
+        Are multidimensional recurrent layers really necessary for handwritten text recognition?
+        In: Document Analysis and Recognition (ICDAR), 2017 14th
+        IAPR International Conference on, vol. 1, pp. 67–72. IEEE (2017)
+        Carlos Mocholí Calvo and Enrique Vidal Ruiz.
+        Development and experimentation of a deep learning system for convolutional and recurrent neural networks
+        Escola Tècnica Superior d’Enginyeria Informàtica, Universitat Politècnica de València, 2018
+    """
+
+    input_data = Input(name="input", shape=input_size)
+
+    cnn = Conv2D(filters=16, kernel_size=(3,3), strides=(1,1), padding="same")(input_data)
+    cnn = BatchNormalization()(cnn)
+    cnn = LeakyReLU(alpha=0.01)(cnn)
+    cnn = MaxPooling2D(pool_size=(2,2), strides=(2,2), padding="valid")(cnn)
+
+    cnn = Conv2D(filters=32, kernel_size=(3,3), strides=(1,1), padding="same")(cnn)
+    cnn = BatchNormalization()(cnn)
+    cnn = LeakyReLU(alpha=0.01)(cnn)
+    cnn = MaxPooling2D(pool_size=(2,2), strides=(2,2), padding="valid")(cnn)
+
+    cnn = Dropout(rate=0.2)(cnn)
+    cnn = Conv2D(filters=48, kernel_size=(3,3), strides=(1,1), padding="same")(cnn)
+    cnn = BatchNormalization()(cnn)
+    cnn = LeakyReLU(alpha=0.01)(cnn)
+    cnn = MaxPooling2D(pool_size=(2,2), strides=(2,2), padding="valid")(cnn)
+
+    cnn = Dropout(rate=0.2)(cnn)
+    cnn = Conv2D(filters=64, kernel_size=(3,3), strides=(1,1), padding="same")(cnn)
+    cnn = BatchNormalization()(cnn)
+    cnn = LeakyReLU(alpha=0.01)(cnn)
+
+    cnn = Dropout(rate=0.2)(cnn)
+    cnn = Conv2D(filters=80, kernel_size=(3,3), strides=(1,1), padding="same")(cnn)
+    cnn = BatchNormalization()(cnn)
+    cnn = LeakyReLU(alpha=0.01)(cnn)
+
+    shape = cnn.get_shape()
+    blstm = Reshape((shape[1], shape[2] * shape[3]))(cnn)
+
+    blstm = Bidirectional(LSTM(units=256, return_sequences=True, dropout=0.5))(blstm)
+    blstm = Bidirectional(LSTM(units=256, return_sequences=True, dropout=0.5))(blstm)
+#     blstm = Bidirectional(LSTM(units=256, return_sequences=True, dropout=0.5))(blstm)
+#     blstm = Bidirectional(LSTM(units=256, return_sequences=True, dropout=0.5))(blstm)
+#     blstm = Bidirectional(LSTM(units=256, return_sequences=True, dropout=0.5))(blstm)
+
+    blstm = Dropout(rate=0.5)(blstm)
+    output_data = Dense(units=d_model, activation="softmax")(blstm)
+
+#     optimizer = RMSprop(learning_rate=learning_rate)
+    optimizer = Adam(learning_rate=learning_rate)
+    
+#     model = Model(inputs=input_data, outputs=output_data)
+#     model.compile(optimizer=optimizer, loss=ctc_loss_lambda_func)
+
+#     return model
+
+    return (input_data, output_data, optimizer)
--- model_training.py
+++ model_training.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
 
-from model import flor, ctc_loss_lambda_func, get_callbacks
+from model import flor, ctc_loss_lambda_func, get_callbacks, minh_model
 from prepare_data import *
 
-
+import itertools
 from tensorflow.keras.models import Model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
@@ -17,7 +17,14 @@ import argparse
 import os
 import datetime
 
-
+def decode_batch(out):
+    result = []
+    for j in range(out.shape[0]):
+        out_best = list(np.argmax(out[j, 2:], 1))
+        out_best = [k for k, g in itertools.groupby(out_best)]
+        outstr = label_to_text(out_best, letters)
+        result.append(outstr)
+    return result
 
 input_size = (1024, 128, 1)
 max_text_length = 143
@@ -25,12 +32,16 @@ letters = " #'()+,-./:0123456789ABCDEFGHIJKLMNOPQRSTUVWXYabcdeghiklmnopqrstwuvxy
 
 
 # Import json labels file
-labels_json_file = './data/labels.json'
-labels_json = pd.read_json(labels_json_file, orient='index', encoding="utf-8").reset_index()
-labels_json.columns = ['name', 'label']
+labels_train_json_file = './data/train.json'
+labels_train_json = pd.read_json(labels_train_json_file, orient='index', encoding="utf-8").reset_index()
+labels_train_json.columns = ['name', 'label']
+
+labels_test_json_file = './data/test.json'
+labels_test_json = pd.read_json(labels_test_json_file, orient='index', encoding="utf-8").reset_index()
+labels_test_json.columns = ['name', 'label']
 
 # Set default variable
-max_len = 69
+max_len = 70
 BATCH_SIZE = 32
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 
@@ -41,21 +52,29 @@ train_folder = pathlib.Path(train_link)
 test_folder = pathlib.Path(test_link)
 
 all_train_path = [str(item) for item in train_folder.glob('*/') if item.is_file()]
-all_train_label = get_label(all_train_path, labels_json, max_len, letters)
+all_train_label = get_label(all_train_path, labels_train_json, max_len, letters)
+
+all_test_path = [str(item) for item in test_folder.glob('*/') if item.is_file()]
+all_test_label = get_label(all_test_path, labels_test_json, max_len, letters)
 
 train_path, valid_path, train_label, valid_label = train_test_split(all_train_path, all_train_label, test_size = 0.3, random_state = 101)
 
 train_ds = prepare_for_training(train_path, train_label, BATCH_SIZE)
 valid_ds = prepare_for_training(valid_path, valid_label, BATCH_SIZE)
+test_ds = prepare_for_training(all_test_path, all_test_label, BATCH_SIZE)
 
 TRAIN_SAMPLES = len(train_label)
 VALID_SAMPLES = len(valid_label)
+TEST_SAMPLES = len(all_test_label)
 
 num_steps_train = tf.math.ceil(float(TRAIN_SAMPLES)/BATCH_SIZE)   
 num_steps_val = tf.math.ceil(float(VALID_SAMPLES)/BATCH_SIZE) 
+num_steps_test = tf.math.ceil(float(TEST_SAMPLES)/BATCH_SIZE) 
 
 
-outs = flor(input_size, 143, 0.001)
+# outs = flor(input_size, 143, 0.001)
+outs = minh_model(input_size, 143)
+
 inputs, outputs, optimizer = outs
 model = Model(inputs=inputs, outputs=outputs)
 model.compile(optimizer=optimizer, loss=ctc_loss_lambda_func)
@@ -70,8 +89,9 @@ start_time = datetime.datetime.now()
 
 history = model.fit(train_ds,
                     steps_per_epoch = num_steps_train,
-                    epochs=5,
+                    epochs=20,
                     validation_data = valid_ds,
+                    callback = callback,
                     validation_steps = num_steps_val)
 
 total_time = datetime.datetime.now() - start_time
@@ -96,3 +116,12 @@ t_corpus = "\n".join([
 with open(os.path.join("./info/train.txt"), "w") as f:
     f.write(t_corpus)
     print(t_corpus)
+    
+
+predictions = model.predict(test_ds, steps=num_steps_test)
+predicts = decode_batch(predictions)
+prediction_file = './info/predictions_1.txt'
+
+with open(prediction_file, "w") as f:
+    for pd, gt in zip(predicts, labels_test_json['label']):
+        f.write("Y {}\nP {}\n".format(gt, pd))
